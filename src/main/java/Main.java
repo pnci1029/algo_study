1a92a2b3748cd722c77bd16345dbe99651354f81
ChHong <pnci1029@gmail.com>
2022-10-18 13:58:00 +0900

    10.18

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,26 @@
 import java.util.*;
-
 class Solution {
-    public String solution(String s) {
-        String answer = "";
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
 
-        String[] split = s.split("");
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
 
-
-        answer += split[0].toUpperCase();
-        for (int i = 0; i < split.length-1; i++) {
-            if (split[i].equals(" ")) {
-                answer +=split[i + 1].toUpperCase();
-            } else {
-                answer += split[i+1].toLowerCase();
-
+            if (citations[i] >= h) {
+                answer = h;
+                break;
             }
-
         }
 
         return answer;
     }
 }
-
 public class Main {
     public static void main(String[] args) {
 
-        String number = "3people unFollowed me";
-        String number1 = "a a a a a              a a a a a ";
-        String number2 = "for the last week";
+        int[] box = {3, 0, 6, 1, 5};
         Solution solution = new Solution();
-        System.out.println(solution.solution(number));
-        System.out.println(solution.solution(number1));
-        System.out.println(solution.solution(number2));
-
-
-
+        System.out.println(solution.solution(box));
     }
 }
