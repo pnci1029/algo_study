cffe078c47d0f3cdb3bb31e7cae7d92eaec02a7e
ChHong <pnci1029@gmail.com>
2022-11-01 13:34:26 +0900

    11.01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int solution(String s) {
+    public int solution(int[] priorities, int location) {
         int answer = 0;
 
-        String[] split = s.split("");
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].contains("z") ||split[i].contains("o") || split[i].contains("t") || split[i].contains("f") || split[i].contains("s") || split[i].contains("e") || split[i].contains("n")) {
-                s = s.replace("zero", "0");
-                s = s.replace("one", "1");
-                s = s.replace("two", "2");
-                s = s.replace("three", "3");
-                s = s.replace("four", "4");
-                s = s.replace("five", "5");
-                s = s.replace("six", "6");
-                s = s.replace("seven", "7");
-                s = s.replace("eight", "8");
-                s = s.replace("nine", "9");
-                break;
+        List<Integer> box = new ArrayList<>();
+        for (int priority : priorities)
+            box.add(priority);
+
+        int count = 0;
+        while (!box.isEmpty()) {
+            Integer target = box.remove(0);
+            if (box.stream().anyMatch(compare -> target < compare)) {
+                box.add(target);
+            } else {
+                count++;
+                if (location == 0) {
+                    break;
+                }
+            }
+            location--;
+            if (location < 0) {
+                location = box.size() - 1;
             }
         }
 
-        answer = Integer.parseInt(s);
-        return answer;
+        return count;
     }
 }
 public class Main {
     public static void main(String[] args) {
 
-        String s = "one4seveneight";
+        int[] box = {1, 1, 9, 1, 1, 1};
+        int n = 0;
         Solution solution = new Solution();
-        System.out.println(solution.solution(s));
+        System.out.println(solution.solution(box,n));
     }
 }
---
+++ src/main/java/hang_algo/_프린터_11_01.java
@@ -0,0 +1,40 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//class Solution {
+//    public int solution(int[] priorities, int location) {
+//        int answer = 0;
+//
+//        List<Integer> box = new ArrayList<>();
+//        for (int priority : priorities)
+//            box.add(priority);
+//
+//        int count = 0;
+//        while (!box.isEmpty()) {
+//            Integer target = box.remove(0);
+//            if (box.stream().anyMatch(compare -> target < compare)) {
+//                box.add(target);
+//            } else {
+//                count++;
+//                if (location == 0) {
+//                    break;
+//                }
+//            }
+//            location--;
+//            if (location < 0) {
+//                location = box.size() - 1;
+//            }
+//        }
+//
+//        return count;
+//    }
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int[] box = {1, 1, 9, 1, 1, 1};
+//        int n = 0;
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(box,n));
+//    }
+//}
