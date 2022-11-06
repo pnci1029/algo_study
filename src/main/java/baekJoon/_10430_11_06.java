d68bda8a623c580dabf789643d08892d7d3b16e5
ChHong <pnci1029@gmail.com>
2022-11-06 12:32:01 +0900

    11.06

---
+++ src/main/java/baekJoon/_10430_11_06.java
@@ -0,0 +1,20 @@
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner data = new Scanner(System.in);
+//        int A = data.nextInt();
+//        int B = data.nextInt();
+//        int C = data.nextInt();
+//
+//        System.out.println((A+B)%C);
+//        System.out.println(((A%C) + (B%C))%C);
+//        System.out.println((A*B)%C);
+//        System.out.println(((A%C) * (B%C))%C);
+//
+////        Solution solution = new Solution();
+////        System.out.println(solution.solution(box));
+//    }
+//}
---
+++ src/main/java/baekJoon/_2588_11_06.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner data = new Scanner(System.in);
+//        int A = data.nextInt();
+//        int B = data.nextInt();
+//
+//        System.out.println(A*(B%10));
+//        System.out.println(A * ((B-(B / 100 * 100))/10));
+//        System.out.println(A*(B/100));
+//        System.out.println(A*B);
+//
+//
+////        Solution solution = new Solution();
+////        System.out.println(solution.solution(box));
+//    }
+//}
