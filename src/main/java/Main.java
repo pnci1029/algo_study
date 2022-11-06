232ea893d83df87ed8f8b9797c65791360a38011
ChHong <pnci1029@gmail.com>
2022-11-06 12:31:50 +0900

    11.06

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,38 @@
-import java.util.Collections;
-import java.util.Stack;
 
-class Solution {
-    public int solution(int[] ingredient) {
-        int answer = 0;
+//class Solution {
+//    public int solution(int[] ingredient) {
+//        int answer = 0;
+//
+//        StringBuilder letter = new StringBuilder();
+//        for (int i = 0; i < ingredient.length; i++) {
+//            letter.append(ingredient[i]);
+//            if (letter.length() > 3 && letter.subSequence(letter.length() - 4, letter.length()).equals("1231")) {
+//                answer++;
+//                letter.delete(letter.length() - 4, letter.length());
+//            }
+//        }
+//
+//
+//        return answer;
+//    }
+//}
 
-        StringBuilder letter = new StringBuilder();
-        for (int i = 0; i < ingredient.length; i++) {
-            letter.append(ingredient[i]);
-            if (letter.length() > 3 && letter.subSequence(letter.length() - 4, letter.length()).equals("1231")) {
-                answer++;
-                letter.delete(letter.length() - 4, letter.length());
-            }
-        }
-
-
-        return answer;
-    }
-}
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] box = {2, 1, 1, 2, 3, 1, 2, 3, 1};
-//        int[] box = {1, 3, 2, 1, 2, 1, 3, 1, 2};
-//        int[] box = {1,2,1,2,3,1,3,1,2,3,1,2,3,1};
-//        int[] box = {1,2,2,3,1};
+
+        Scanner data = new Scanner(System.in);
+        int A = data.nextInt();
+        int B = data.nextInt();
+
+        System.out.println(A*(B%10));
+        System.out.println(A * ((B-(B / 100 * 100))/10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
 
 
-
-        Solution solution = new Solution();
-        System.out.println(solution.solution(box));
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(box));
     }
 }
