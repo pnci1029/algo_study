0674dfbec604480714a853334d5f468bee68205a
ChHong <pnci1029@gmail.com>
2022-11-11 09:25:49 +0900

    11.11

---
+++ src/main/java/programmers/_멀리뛰기_11_11.java
@@ -0,0 +1,31 @@
+//class Solution {
+//    public long solution(int n) {
+//        long answer = 0;
+//
+//        if (n < 3) {
+//            return n;
+//        } else {
+//            long left = 1;
+//            long right = 2;
+//            for (int i = 3; i <= n; i++) {
+//                answer = (left + right) % 1234567;
+//                if (i % 2 != 0) {
+//                    left = answer;
+//                } else {
+//                    right = answer;
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int a =1;
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a));
+//    }
+//}
