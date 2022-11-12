c6f41114b18a1de52134e9238bad024505e82d47
ChHong <pnci1029@gmail.com>
2022-11-12 12:05:49 +0900

    11.12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,80 @@
 class Solution {
-    public long solution(int n) {
-        long answer = 0;
+    public int solution(String word) {
+        int answer = 0;
+        String[] box = {"A", "E", "I", "O", "U"};
+        int[] numbBox = {781, 156, 31, 6, 1};
 
-        if (n < 3) {
-            return n;
-        } else {
-            long left = 1;
-            long right = 2;
-            for (int i = 3; i <= n; i++) {
-                answer = (left + right) % 1234567;
-                if (i % 2 != 0) {
-                    left = answer;
-                } else {
-                    right = answer;
-                }
+        String[] split = word.split("");
+
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].equals("A")) {
+
             }
         }
+
+        switch (split.length) {
+            case 1:
+                for (int i = 0; i < box.length; i++) {
+                    if (split[0].equals(box[i])) {
+                        answer = 781 * i + 1;
+                    }
+                }break;
+            case 2:
+                for (int i = 0; i < box.length; i++) {
+                    if (split[0].equals(box[i])) {
+                        answer += 781 * i + 1;
+                    }
+                    if (split[1].equals(box[i])) {
+                        answer += 156 * i + 1;
+                    }
+                }break;
+            case 3:
+                for (int i = 0; i < box.length; i++) {
+                    if (split[0].equals(box[i])) {
+                        answer += 781 * i + 1;
+                    }
+                    if (split[1].equals(box[i])) {
+                        answer += 156 * i + 1;
+                    }
+                    if (split[2].equals(box[i])) {
+                        answer += 31 * i + 1;
+                    }
+                }break;
+            case 4:
+                for (int i = 0; i < box.length; i++) {
+                    if (split[0].equals(box[i])) {
+                        answer += 781 * i + 1;
+                    }
+                    if (split[1].equals(box[i])) {
+                        answer += 156 * i + 1;
+                    }
+                    if (split[2].equals(box[i])) {
+                        answer += 31 * i + 1;
+                    }
+                    if (split[3].equals(box[i])) {
+                        answer += 6 * i + 1;
+                    }
+                }break;
+            case 5:
+                for (int i = 0; i < box.length; i++) {
+                    if (split[0].equals(box[i])) {
+                        answer += 781 * i + 1;
+                    }
+                    if (split[1].equals(box[i])) {
+                        answer += 156 * i + 1;
+                    }
+                    if (split[2].equals(box[i])) {
+                        answer += 31 * i + 1;
+                    }
+                    if (split[3].equals(box[i])) {
+                        answer += 6 * i + 1;
+                    }
+                    if (split[4].equals(box[i])) {
+                        answer +=  i+1;
+                    }
+                }break;
+        }
+
 
         return answer;
     }
@@ -23,8 +82,9 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
+        String a = "AAAAE";
+//        String a = "A";
 
-        int a =1;
         Solution solution = new Solution();
         System.out.println(solution.solution(a));
     }
---
+++ src/main/java/programmers/_모음사전_11_12.java
@@ -0,0 +1,91 @@
+//class Solution {
+//    public int solution(String word) {
+//        int answer = 0;
+//        String[] box = {"A", "E", "I", "O", "U"};
+//        int[] numbBox = {781, 156, 31, 6, 1};
+//
+//        String[] split = word.split("");
+//
+//        for (int i = 0; i < split.length; i++) {
+//            if (split[i].equals("A")) {
+//
+//            }
+//        }
+//
+//        switch (split.length) {
+//            case 1:
+//                for (int i = 0; i < box.length; i++) {
+//                    if (split[0].equals(box[i])) {
+//                        answer = 781 * i + 1;
+//                    }
+//                }break;
+//            case 2:
+//                for (int i = 0; i < box.length; i++) {
+//                    if (split[0].equals(box[i])) {
+//                        answer += 781 * i + 1;
+//                    }
+//                    if (split[1].equals(box[i])) {
+//                        answer += 156 * i + 1;
+//                    }
+//                }break;
+//            case 3:
+//                for (int i = 0; i < box.length; i++) {
+//                    if (split[0].equals(box[i])) {
+//                        answer += 781 * i + 1;
+//                    }
+//                    if (split[1].equals(box[i])) {
+//                        answer += 156 * i + 1;
+//                    }
+//                    if (split[2].equals(box[i])) {
+//                        answer += 31 * i + 1;
+//                    }
+//                }break;
+//            case 4:
+//                for (int i = 0; i < box.length; i++) {
+//                    if (split[0].equals(box[i])) {
+//                        answer += 781 * i + 1;
+//                    }
+//                    if (split[1].equals(box[i])) {
+//                        answer += 156 * i + 1;
+//                    }
+//                    if (split[2].equals(box[i])) {
+//                        answer += 31 * i + 1;
+//                    }
+//                    if (split[3].equals(box[i])) {
+//                        answer += 6 * i + 1;
+//                    }
+//                }break;
+//            case 5:
+//                for (int i = 0; i < box.length; i++) {
+//                    if (split[0].equals(box[i])) {
+//                        answer += 781 * i + 1;
+//                    }
+//                    if (split[1].equals(box[i])) {
+//                        answer += 156 * i + 1;
+//                    }
+//                    if (split[2].equals(box[i])) {
+//                        answer += 31 * i + 1;
+//                    }
+//                    if (split[3].equals(box[i])) {
+//                        answer += 6 * i + 1;
+//                    }
+//                    if (split[4].equals(box[i])) {
+//                        answer +=  i+1;
+//                    }
+//                }break;
+//        }
+//
+//
+//        return answer;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        String a = "AAAAE";
+////        String a = "A";
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a));
+//    }
+//}
