775b006c2d5fc66b4e4b56d19cf7c0522bf927d9
ChHong <pnci1029@gmail.com>
2022-11-25 15:01:12 +0900

    11.25

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,36 @@
-//import java.util.*;
-//
-//class Solution {
-//    public static Scanner scanner = new Scanner(System.in);
-//    public List<Integer> solution(int a, int[] speeds) {
-//
-//        scanner.nextInt();
-//
-//
-//        Map<Integer, Integer> box = new HashMap<>();
-//
-//        List<Integer> result = new ArrayList<>();
-//        for (int speed : speeds) {
-//            box.put(speed, box.getOrDefault(speed, 0) + 1);
-//        }
-//
-//        for (int speed : speeds) {
-//            if (box.get(speed) == 1) {
-//                result.add(speed);
-//            }
-//        }
-//        Collections.sort(result);
-//
-//
-//        return result;
-//    }
-//}
-//
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        int a = 5;
-//        int[] b = {4, 3, 3, 2, 6};
-////        int[] b = {1,2,3,4,5,4,3,2,1,2};
-//
-//
-//        Solution solution = new Solution();
-//        System.out.println(solution.solution(a, b));
-//    }
-//}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+
+
+        List<Integer> numb = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            numb.add(score[i]);
+            Collections.sort(numb);
+            if (numb.size() > k) {
+                numb.remove(0);
+            }
+            answer[i] = numb.get(0);
+        }
+
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int[] b = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution(a, b));
+    }
+}
---
+++ src/main/java/programmers/_11_25_명예의전당.java
@@ -0,0 +1,36 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//class Solution {
+//    public int[] solution(int k, int[] score) {
+//        int[] answer = new int[score.length];
+//
+//
+//        List<Integer> numb = new ArrayList<>();
+//
+//        for (int i = 0; i < score.length; i++) {
+//            numb.add(score[i]);
+//            Collections.sort(numb);
+//            if (numb.size() > k) {
+//                numb.remove(0);
+//            }
+//            answer[i] = numb.get(0);
+//        }
+//
+//
+//        return answer;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int a = 4;
+//        int[] b = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+//
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a, b));
+//    }
+//}
