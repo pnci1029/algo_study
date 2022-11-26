19b5124ba554a2f3269d75d4de0dc53dcd7f0f7a
ChHong <pnci1029@gmail.com>
2022-11-26 12:21:59 +0900

    11.26

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
 
+        Map<Integer, Integer> box = new HashMap<>();
 
-        List<Integer> numb = new ArrayList<>();
+        for (int i : tangerine) {
+            box.put(i, box.getOrDefault(i, 0) + 1);
+        }
 
-        for (int i = 0; i < score.length; i++) {
-            numb.add(score[i]);
-            Collections.sort(numb);
-            if (numb.size() > k) {
-                numb.remove(0);
+        List<Integer> numbBox = new ArrayList<>();
+
+        for (Integer integer : box.keySet()) {
+            numbBox.add(box.get(integer));
+        }
+        Collections.sort(numbBox);
+        Collections.reverse(numbBox);
+
+        for (Integer integer : numbBox) {
+            if (integer >= k) {
+                answer++;
+                break;
+            } else {
+                k -= integer;
+                answer++;
             }
-            answer[i] = numb.get(0);
         }
 
 
+
+        System.out.println("numbBox = " + numbBox.toString());
         return answer;
     }
 }
@@ -26,8 +38,12 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
 
-        int a = 4;
-        int[] b = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+        int a = 6;
+        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
+//        int a = 2;
+//        int[] b = {1, 1, 1, 1, 2, 2, 2, 3};
+//        int a = 4;
+//        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
 
 
         Solution solution = new Solution();
---
+++ src/main/java/programmers/_11_25_귤고르기.java
@@ -0,0 +1,52 @@
+//import java.util.*;
+//
+//class Solution {
+//    public int solution(int k, int[] tangerine) {
+//        int answer = 0;
+//
+//        Map<Integer, Integer> box = new HashMap<>();
+//
+//        for (int i : tangerine) {
+//            box.put(i, box.getOrDefault(i, 0) + 1);
+//        }
+//
+//        List<Integer> numbBox = new ArrayList<>();
+//
+//        for (Integer integer : box.keySet()) {
+//            numbBox.add(box.get(integer));
+//        }
+//        Collections.sort(numbBox);
+//        Collections.reverse(numbBox);
+//
+//        for (Integer integer : numbBox) {
+//            if (integer >= k) {
+//                answer++;
+//                break;
+//            } else {
+//                k -= integer;
+//                answer++;
+//            }
+//        }
+//
+//
+//
+//        System.out.println("numbBox = " + numbBox.toString());
+//        return answer;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int a = 6;
+//        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
+////        int a = 2;
+////        int[] b = {1, 1, 1, 1, 2, 2, 2, 3};
+////        int a = 4;
+////        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
+//
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a, b));
+//    }
+//}
