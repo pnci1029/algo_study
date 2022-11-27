d7aae654081d5aabe893fdd07462ebc9633941bf
ChHong <pnci1029@gmail.com>
2022-11-27 09:40:30 +0900

    11.27

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,24 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int k, int[] tangerine) {
-        int answer = 0;
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
-        Map<Integer, Integer> box = new HashMap<>();
-
-        for (int i : tangerine) {
-            box.put(i, box.getOrDefault(i, 0) + 1);
-        }
-
-        List<Integer> numbBox = new ArrayList<>();
-
-        for (Integer integer : box.keySet()) {
-            numbBox.add(box.get(integer));
-        }
-        Collections.sort(numbBox);
-        Collections.reverse(numbBox);
-
-        for (Integer integer : numbBox) {
-            if (integer >= k) {
-                answer++;
-                break;
-            } else {
-                k -= integer;
-                answer++;
+        for(int i = 0 ; i < arr1.length ; ++i){
+            for(int j = 0 ; j < arr2[0].length ; ++j){
+                for(int k = 0 ; k < arr1[0].length ; ++k) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
             }
         }
 
-
-
-        System.out.println("numbBox = " + numbBox.toString());
         return answer;
     }
 }
 
+
 public class Main {
     public static void main(String[] args) {
-
-        int a = 6;
-        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
-//        int a = 2;
-//        int[] b = {1, 1, 1, 1, 2, 2, 2, 3};
-//        int a = 4;
-//        int[] b = {1, 3, 2, 5, 4, 5, 2, 3};
+        int[][] a = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] b = {{3, 3}, {3, 3}};
 
 
         Solution solution = new Solution();
---
+++ src/main/java/programmers/_11_26_행렬의곱셈.java
@@ -0,0 +1,27 @@
+//class Solution {
+//    public int[][] solution(int[][] arr1, int[][] arr2) {
+//        int[][] answer = new int[arr1.length][arr2[0].length];
+//
+//        for(int i = 0 ; i < arr1.length ; ++i){
+//            for(int j = 0 ; j < arr2[0].length ; ++j){
+//                for(int k = 0 ; k < arr1[0].length ; ++k) {
+//                    answer[i][j] += arr1[i][k] * arr2[k][j];
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int[][] a = {{1, 4}, {3, 2}, {4, 1}};
+//        int[][] b = {{3, 3}, {3, 3}};
+//
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a, b));
+//    }
+//}
