9647280e63dd497271bf946362bf5ef3b91b97ed
ChHong <pnci1029@gmail.com>
2022-11-28 19:48:24 +0900

    11.28

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
+        int[][] answer = new int [arr1.length][arr2[0].length];
 
-        for(int i = 0 ; i < arr1.length ; ++i){
-            for(int j = 0 ; j < arr2[0].length ; ++j){
-                for(int k = 0 ; k < arr1[0].length ; ++k) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j];
-                }
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
 
@@ -17,7 +15,7 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-        int[][] a = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] a = {{1, 4}, {3, 2}};
         int[][] b = {{3, 3}, {3, 3}};
 
 
---
+++ src/main/java/programmers/_11_27_행렬의덧셈.java
@@ -0,0 +1,25 @@
+//class Solution {
+//    public int[][] solution(int[][] arr1, int[][] arr2) {
+//        int[][] answer = new int [arr1.length][arr2[0].length];
+//
+//        for (int i = 0; i < arr1.length; i++) {
+//            for (int j = 0; j < arr1[0].length; j++) {
+//                answer[i][j] = arr1[i][j] + arr2[i][j];
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int[][] a = {{1, 4}, {3, 2}};
+//        int[][] b = {{3, 3}, {3, 3}};
+//
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a, b));
+//    }
+//}
