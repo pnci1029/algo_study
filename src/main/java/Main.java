21ff75974b04cbe1e9f507204d52ec1a72db0c03
ChHong <pnci1029@gmail.com>
2022-11-30 20:11:20 +0900

    11.30

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,60 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
-    public int solution(int n) {
-        int ans = 0;
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
 
-        int numb = n;
-        while (true) {
+        List<String> box = new ArrayList<>();
 
-            if (numb == 2) {
-                ans++;
-                break;
-            }
-            if (numb % 2 == 1) {
-                ans++;
-                numb--;
-            }
-            if (numb == 0) {
-                break;
-            }
-            numb /= 2;
+        char[] chars = skill.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            box.add(String.valueOf(chars[i]));
         }
 
+        for (String skill_tree : skill_trees) {
+            String[] split = skill_tree.split("");
 
-        return ans;
+            String result = "";
+            String compare = "";
+
+            for (int i = 0; i < split.length; i++) {
+                for (String s : box) {
+                    if (split[i].equals(s)) {
+                        result += split[i];
+                        break;
+                    }
+
+                }
+            }
+            if (result.length() != 0) {
+                for (int i = 0; i < result.length(); i++) {
+                    compare += box.get(i);
+                }
+            }
+            if (result.equals(compare)) {
+
+                answer++;
+            }
+//            System.out.println("compare = " + compare);
+//            System.out.println("result = " + result);
+        }
+        return answer;
     }
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        int a =6;
+        String a ="CBD";
+        String[] b = {"BACDE", "CBADF", "AECB", "BDA"};
+
+//        String a = "CBD";
+//        String[] b = {"CXF", "ASF", "BDF", "CEFD"};
 
 
         Solution solution = new Solution();
-        System.out.println(solution.solution(a));
+        System.out.println(solution.solution(a,b));
     }
 }
---
+++ src/main/java/programmers/_11_29_스킬트리.java
@@ -0,0 +1,60 @@
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//class Solution {
+//    public int solution(String skill, String[] skill_trees) {
+//        int answer = 0;
+//
+//        List<String> box = new ArrayList<>();
+//
+//        char[] chars = skill.toCharArray();
+//        for (int i = 0; i < chars.length; i++) {
+//            box.add(String.valueOf(chars[i]));
+//        }
+//
+//        for (String skill_tree : skill_trees) {
+//            String[] split = skill_tree.split("");
+//
+//            String result = "";
+//            String compare = "";
+//
+//            for (int i = 0; i < split.length; i++) {
+//                for (String s : box) {
+//                    if (split[i].equals(s)) {
+//                        result += split[i];
+//                        break;
+//                    }
+//
+//                }
+//            }
+//            if (result.length() != 0) {
+//                for (int i = 0; i < result.length(); i++) {
+//                    compare += box.get(i);
+//                }
+//            }
+//            if (result.equals(compare)) {
+//
+//                answer++;
+//            }
+////            System.out.println("compare = " + compare);
+////            System.out.println("result = " + result);
+//        }
+//        return answer;
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        String a ="CBD";
+//        String[] b = {"BACDE", "CBADF", "AECB", "BDA"};
+//
+////        String a = "CBD";
+////        String[] b = {"CXF", "ASF", "BDF", "CEFD"};
+//
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a,b));
+//    }
+//}
