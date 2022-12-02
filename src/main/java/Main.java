a159c8fa023385c34e982ff159a1e204e5c69b1d
ChHong <pnci1029@gmail.com>
2022-12-02 13:24:40 +0900

    12.02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,83 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int N, int[] stages) {
+    public int solution(String s) {
+        int answer = 0;
 
-        List<Integer> box = new ArrayList<>();
-        List<Double> stageFailureRate = new ArrayList<>();
-        List<Integer> zeroCollect = new ArrayList<>();
-        for (int stage : stages) {
-            box.add(stage);
-        }
+        String[] split = s.split("");
 
-        int stageNumb = 1;
-        double totalCount =  stages.length;
-        while (true) {
+        String word = "";
 
-            int failureRate = 0;
-            for (int i = 0; i < box.size(); i++) {
-                if (box.get(i) == stageNumb) {
-                    failureRate++;
-                }
+        int left = 0;
+        int right = 0;
+
+        word = split[0];
+        int count = 0;
+        for (int i = 0; i < split.length; i++) {
+            if (count == 0) {
+                word = split[i];
             }
-
-            if (totalCount != 0) {
-                stageFailureRate.add(failureRate / totalCount);
-
+            if (split[i].equals(word)) {
+                left++;
             } else {
-//                zeroCollect.add(i + 1);
-                stageFailureRate.add(0.0);
+                right++;
             }
-            System.out.println("stageFailureRate = " + stageFailureRate);
-            totalCount -= failureRate;
-
-            stageNumb++;
-            if (stageNumb > N) {
-                break;
-            }
-        }
-        box.clear();
-        int compare = stageFailureRate.size()-1;
-        while (true) {
-            for (int i = stageFailureRate.size()-2; i >= 0; i--) {
-                if (stageFailureRate.get(compare) <= stageFailureRate.get(i)) {
-                    compare = i;
-                }
-            }
-            box.add(compare+1);
-
-            stageFailureRate.set(compare,-1.0);
-
-            if (box.size() == stageFailureRate.size()-1) {
-                for (int i = 0; i < stageFailureRate.size(); i++) {
-                    if (stageFailureRate.get(i) != -1.0) {
-                        box.add(i + 1);
-                    }
-                }
-
-                break;
+            count++;
+            if (left == right) {
+                answer++;
+                count = 0;
+                left = 0;
+                right =0;
             }
         }
 
+        if (left != 0 || right != 0) {
+            return answer + 1;
+        } else {
+            return answer;
+        }
 
-        return box;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-//        int b = 5;
-//        int[] a = {2, 1, 2, 6, 2, 4, 3, 3};
 
-        int b = 4;
-        int[] a = {4,4,4,4};
-
-//        int b = 5;
-//        int[] a = {3,3,3,3};
+        String a = "abbaaa";
 
         Solution solution = new Solution();
-        System.out.println(solution.solution(b, a));
+        System.out.println(solution.solution(a));
     }
 }
---
+++ src/main/java/programmers/_12_02_문자열나누기.java
@@ -0,0 +1,51 @@
+//
+//class Solution {
+//    public int solution(String s) {
+//        int answer = 0;
+//
+//        String[] split = s.split("");
+//
+//        String word = "";
+//
+//        int left = 0;
+//        int right = 0;
+//
+//        word = split[0];
+//        int count = 0;
+//        for (int i = 0; i < split.length; i++) {
+//            if (count == 0) {
+//                word = split[i];
+//            }
+//            if (split[i].equals(word)) {
+//                left++;
+//            } else {
+//                right++;
+//            }
+//            count++;
+//            if (left == right) {
+//                answer++;
+//                count = 0;
+//                left = 0;
+//                right =0;
+//            }
+//        }
+//
+//        if (left != 0 || right != 0) {
+//            return answer + 1;
+//        } else {
+//            return answer;
+//        }
+//
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//
+//        String a = "abbaaa";
+//
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a));
+//    }
+//}
