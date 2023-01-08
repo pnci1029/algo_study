cc85c125f2d7632cdf27eada6129315bb3c06475
ChHong <pnci1029@gmail.com>
2023-01-08 13:52:07 +0900

    23_01_08

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,73 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
+
 class Solution {
-    public List<Long> solution(long[] numbers) {
-        List<Long> result = new ArrayList<>();
-
-        for (Long value : numbers) {
-            String target = Long.toBinaryString(value);
-
-            int count = 0;
-            String answer = "";
-            if (target.length() >= 2) {
-                String numb = target.substring(target.length() - 2);
-                if (numb.equals("00") || numb.equals("10")) {
-                    result.add(value + 1);
-                } else if (numb.equals("01")) {
-                    result.add(value + 1);
+    public String solution(String polynomial) {
+        String answer = "";
+        int result = 0;
+        int numb = 0;
+        String[] split = polynomial.split(" ");
+        for (String s : split) {
+            if (s.contains("x")) {
+                if (s.equals("x")) {
+                    result += 1;
                 } else {
-                    String[] split = target.split("");
-                    int oneCount = split.length - 1;
-                    while (true) {
-                        if (split[oneCount].equals("1")) {
-                            count++;
-                        } else {
-                            break;
-                        }
-                        oneCount--;
-                        if (count >= split.length) {
-                            break;
-                        }
-                    }
-
-                    if (count == split.length) {
-                        answer = "10";
-                        count--;
-                        while (count > 0) {
-                            answer += "1";
-                            count--;
-                        }
-                        result.add(Long.parseLong(answer, 2));
-                        /**
-                         * 1011 -> 1101
-                         * 10011 -> 10101
-                         */
-                    } else {
-                        answer += target.substring(0, split.length - count - 1) + "10";
-                        count--;
-                        while (count > 0) {
-                            answer += "1";
-                            count--;
-                        }
-
-                        result.add(Long.parseLong(answer, 2));
-                    }
+                    String value = s.replace("x", "");
+                    result += Integer.parseInt(value);
                 }
+            } else if (!s.equals("+")) {
+                numb += Integer.parseInt(s);
+            }
+        }
+        if (result == 1) {
+            if (numb != 0) {
+                answer = "x + " + numb;
+                return answer;
             } else {
-                result.add(value + 1);
+                answer = "x";
+                return answer;
             }
         }
 
-        return result;
+        if (numb != 0 && result != 0) {
+            answer = result + "x + " + numb;
+        } else if (numb == 0) {
+            answer = result + "x";
+        } else if (result == 0) {
+            answer = String.valueOf(numb);
+        }
+        return answer;
     }
 }
 
-
 public class Main {
     public static void main(String[] args) {
+        String a = "7 + x";
 
-//        long[] a = {2, 1015};
-        long[] a = {1001, 337, 0, 1, 333, 673, 343, 221, 898, 997, 121, 1015, 665, 779, 891, 421, 222, 256, 512, 128, 100};
         Solution solution = new Solution();
         System.out.println(solution.solution(a));
 
---
+++ src/main/java/programmers/_12_24_2개이하로_다른비트.java
@@ -0,0 +1,75 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//class Solution {
+//    public List<Long> solution(long[] numbers) {
+//        List<Long> result = new ArrayList<>();
+//
+//        for (Long value : numbers) {
+//            String target = Long.toBinaryString(value);
+//
+//            int count = 0;
+//            String answer = "";
+//            if (target.length() >= 2) {
+//                String numb = target.substring(target.length() - 2);
+//                if (numb.equals("00") || numb.equals("10")) {
+//                    result.add(value + 1);
+//                } else if (numb.equals("01")) {
+//                    result.add(value + 1);
+//                } else {
+//                    String[] split = target.split("");
+//                    int oneCount = split.length - 1;
+//                    while (true) {
+//                        if (split[oneCount].equals("1")) {
+//                            count++;
+//                        } else {
+//                            break;
+//                        }
+//                        oneCount--;
+//                        if (count >= split.length) {
+//                            break;
+//                        }
+//                    }
+//
+//                    if (count == split.length) {
+//                        answer = "10";
+//                        count--;
+//                        while (count > 0) {
+//                            answer += "1";
+//                            count--;
+//                        }
+//                        result.add(Long.parseLong(answer, 2));
+//                        /**
+//                         * 1011 -> 1101
+//                         * 10011 -> 10101
+//                         */
+//                    } else {
+//                        answer += target.substring(0, split.length - count - 1) + "10";
+//                        count--;
+//                        while (count > 0) {
+//                            answer += "1";
+//                            count--;
+//                        }
+//
+//                        result.add(Long.parseLong(answer, 2));
+//                    }
+//                }
+//            } else {
+//                result.add(value + 1);
+//            }
+//        }
+//
+//        return result;
+//    }
+//}
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+////        long[] a = {2, 1015};
+//        long[] a = {1001, 337, 0, 1, 333, 673, 343, 221, 898, 997, 121, 1015, 665, 779, 891, 421, 222, 256, 512, 128, 100};
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution(a));
+//
+//    }
+//}
