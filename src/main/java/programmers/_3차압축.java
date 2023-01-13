786f7d6f63002474823fa707e19a0e26e7cbf787
ChHong <pnci1029@gmail.com>
2023-01-13 20:35:12 +0900

    23_01_13

---
+++ src/main/java/programmers/_3차압축.java
@@ -0,0 +1,26 @@
+//import java.util.*;
+//
+//class Solution {
+//    public int[] solution(String msg) {
+//        int[] answer = {};
+//
+//        Map<String,Integer> map = new HashMap<>();
+//        List<Integer> box = new ArrayList<>();
+//
+//        for (int i = 65; i <= 90; i++) {
+//            map.put(String.valueOf((char) i), i - 64);
+//        }
+//
+//        String[] split = msg.split("");
+//
+//        for (int i = 0; i < split.length; i++) {
+//            if (map.containsKey(split[i])) {
+//                map.put()
+//            }
+//        }
+//
+//        System.out.println("box = " + box);
+//        System.out.println("ma = " + map);
+//        return answer;
+//    }
+//}
