4be5edd363e08454ebce206cd163c95acb400de3
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-04-30 17:41:46 +0900

    Create 23_04_30_18870_좌표압축.java

---
+++ src/main/java/baekJoon/23_04_30_18870_좌표압축.java
@@ -0,0 +1,36 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+public class BackJoon {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int length = sc.nextInt();
+
+        int[] arr = new int[length];
+        int[] arr2 = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = arr2[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], count);
+                count++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr2) {
+            sb.append(map.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
