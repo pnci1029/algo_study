2601dc990cafd5bd8733551ba269de5fd7145828
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-08-01 22:40:19 +0900

    Create 23_08_01_N과M2.java

---
+++ src/main/java/baekJoon/23_08_01_N과M2.java
@@ -0,0 +1,39 @@
+package com.example.tdd_prac;
+
+import java.io.IOException;
+import java.util.*;
+
+public class BackJoon {
+
+    public static int[] arr;
+    public static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        M = in.nextInt();
+
+        arr = new int[M];
+
+        dfs(1, 0);
+
+    }
+
+    public static void dfs(int value, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = value; i <= N; i++) {
+
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+
+        }
+    }
+}
