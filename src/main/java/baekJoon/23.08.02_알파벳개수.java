dee55835663184e2e35c8a0cfe53f29735397bd1
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-08-02 22:01:08 +0900

    Create 23.08.02_알파벳개수.java

---
+++ src/main/java/baekJoon/23.08.02_알파벳개수.java
@@ -0,0 +1,31 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+
+public class BackJoon {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String next = in.next();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 97; i <= 122; i++) {
+            char a = (char) i;
+            map.put(String.valueOf(a), 0);
+        }
+
+        String[] split = next.split("");
+
+        for (String value : split) {
+            map.put(value, map.get(value) + 1);
+        }
+
+        for (Integer value : map.values()) {
+            System.out.print(value+" ");
+        }
+
+    }
+
+}
