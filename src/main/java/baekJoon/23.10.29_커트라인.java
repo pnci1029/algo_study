fdb3a05177b7885085d747ed94ed14467c22479a
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-10-29 10:22:15 +0900

    Create 23.10.29_커트라인.java

---
+++ src/main/java/baekJoon/23.10.29_커트라인.java
@@ -0,0 +1,32 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+
+/**
+ 5 2
+ 100 76 85 93 98
+ */
+
+public class BackJoon {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int length = in.nextInt();
+        int target = in.nextInt();
+
+        int[] box = new int[length];
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+//            box[i] = in.nextInt();
+            arr.add(in.nextInt());
+        }
+
+        Collections.sort(arr);
+        Collections.reverse(arr);
+
+        System.out.println(arr.get(target-1));
+    }
+}
+
