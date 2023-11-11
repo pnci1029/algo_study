ad22c06e50583f9b69a377ef70e2c56913933e78
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-11-11 16:01:58 +0900

    Create 23.11.11_이항계수1.java

---
+++ src/main/java/baekJoon/23.11.11_이항계수1.java
@@ -0,0 +1,39 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+
+/**
+ 5 2
+ 8 4
+ */
+
+public class BackJoon {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int first = in.nextInt();
+        int second = in.nextInt();
+
+        int n =1;
+        int c =1;
+        int k =1;
+        int valueA = first - second;
+        for (int i = first; i >= 1; i--) {
+            n *= i;
+        }
+
+        for (int i = valueA; i >= 1; i--) {
+            c *= i;
+        }
+        for (int i = second; i >= 1; i--) {
+            k *= i;
+        }
+
+        System.out.println(n/(c*k));
+
+
+    }
+}
+
