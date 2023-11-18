f60d3db0937373a453d0e2fe327860448f021268
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-11-18 12:34:10 +0900

    Create 23.11.18_파일정리.java

---
+++ src/main/java/baekJoon/23.11.18_파일정리.java
@@ -0,0 +1,45 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+
+/**
+ 8
+ sbrus.txt
+ spc.spc
+ acm.icpc
+ korea.icpc
+ sample.txt
+ hello.world
+ sogang.spc
+ example.txt
+ */
+
+public class BackJoon {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int length = in.nextInt();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < length; i++) {
+            String value = in.next();
+            int index = value.indexOf('.');
+            String target = value.substring(index + 1);
+
+            map.put(target, map.getOrDefault(target, 0) + 1);
+        }
+        List<String> arr = new ArrayList<>(map.keySet());
+        Collections.sort(arr);
+//        System.out.println(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s).append(" ").append(map.get(s)).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
+
