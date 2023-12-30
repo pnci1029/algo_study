f3a0e82850b57220b3740d99d80ff318ee383474
ChangHyung Hong <81909140+pnci1029@users.noreply.github.com>
2023-12-30 21:34:03 +0900

    Create 23.12.30_요세푸스문제.java

---
+++ src/main/java/baekJoon/23.12.30_요세푸스문제.java
@@ -0,0 +1,37 @@
+package com.example.tdd_prac;
+
+import java.util.*;
+
+/**
+ 7 3
+
+ */
+
+public class BackJoon {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int length = in.nextInt();
+        int range = in.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= length; i++) {
+            queue.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (queue.size() != 1) {
+            for (int i = 0; i < range - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
+    }
+
+
+}
