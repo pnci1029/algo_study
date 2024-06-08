ba1b0dac4c0f6fc2ad8ef1778620f9271827e511
chhong <81909140+pnci1029@users.noreply.github.com>
2024-06-08 16:37:32 +0900

    Update ___24.06.08_미로탐색.java

--- src/main/java/baekJoon/___24.06.08_미로탐색.java
+++ src/main/java/baekJoon/___24.06.08_미로탐색.java
@@ -1,5 +1,3 @@
-package com.example.jenkinstest;
-
 import java.util.*;
 
 /**
@@ -14,6 +12,7 @@ public class BackJoon {
     static int M;
     static int[][] board;
     static Queue<Pair> queue;
+    // 움직인 거리만큼 카운트 할거라 int 배열로 설정
     static int[][] visit;
     static int[] dx;
     static int[] dy;
@@ -41,44 +40,35 @@ public class BackJoon {
 
         }
 
-        int count = 0;
-        int max = 0;
-        int area = 0;
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
+                // 탐색 가능 경로 체크
                 if (visit[i][j] == -1 || board[i][j] == 0) {
                     continue;
                 }
 
-                count++;
                 queue.offer(new Pair(i, j));
-//                visit[i][j]= 0;
 
-                area = 0;
                 while (!queue.isEmpty()) {
                     Pair p = queue.poll();
-                    area++;
 
+                    // 현재 경로에서 상하좌우 체크
                     for (int k = 0; k < 4; k++) {
                         int nX = p.x + dx[k];
                         int nY = p.y + dy[k];
 
+                        // 배열크기를 벗어나거나 - 로 내려가는 경우 체크
                         if (nX < 0 || nY < 0 || nX >= N || nY >= M) {
                             continue;
                         }
 
-
+                        // 탐색 가능 경로일 경우 visit에 이전 위치 + 1 처리
                         if (visit[nX][nY] ==0 && board[nX][nY] == 1) {
                             queue.offer(new Pair(nX, nY));
                             visit[nX][nY] = visit[p.x][p.y]+1;
                         }
                     }
                 }
-                if (area > max) {
-                    max = area;
-                }
-
             }
         }
 
