c00b0f997df5ccca8b29e43b2d2a86f699568b98
chhong <81909140+pnci1029@users.noreply.github.com>
2024-06-23 12:44:11 +0900

    Update ___24.06.23_캠핑V2.java

--- src/main/java/baekJoon/___24.06.23_캠핑.java
+++ src/main/java/baekJoon/___24.06.23_캠핑.java
@@ -44,3 +44,45 @@ public class BackJoon {
 
     }
 }
+
+
+import java.util.*;
+
+/**
+
+ 5 8 20
+ 5 8 17
+ 2 8 20
+ 0 0 0
+ */
+public class BackJoonV2 {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int caseNumber = 0;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            caseNumber++;
+            int ref = in.nextInt();
+            int rule = in.nextInt();
+            int given = in.nextInt();
+            if (ref == rule && rule == given && rule == 0) {
+                break;
+            }
+            int result = 0;
+
+
+            while (given > rule) {
+                given -= rule;
+                result += ref;
+            }
+            result += Math.min(given, ref);
+            sb.append("Case ").append(caseNumber).append(": ").append(result).append("\n");
+
+        }
+        System.out.println(sb);
+
+    }
+}
