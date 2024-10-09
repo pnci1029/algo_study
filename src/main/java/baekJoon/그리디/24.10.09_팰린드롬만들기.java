2d8ff29d27c9b091a5b9061a90f90c7142246348
chhong <81909140+pnci1029@users.noreply.github.com>
2024-10-09 14:44:39 +0900

    Update 24.10.09_팰린드롬만들기.java

--- src/main/java/baekJoon/그리디/24.10.09_팰린드롬만들기.java
+++ src/main/java/baekJoon/그리디/24.10.09_팰린드롬만들기.java
@@ -3,11 +3,10 @@ package com.example.vargenerator.api.controller;
 import java.util.*;
 
 /**
- *
- AABB
- AAABB
- ABACABA
- ABCD
+ * AABB
+ * AAABB
+ * ABACABA
+ * ABCD
  */
 public class Solve {
     public static void main(String[] args) {
@@ -18,7 +17,7 @@ public class Solve {
 
         Map<String, Integer> box2 = new HashMap<>();
         Map<String, Integer> box = new TreeMap<>();
-        Arrays.stream(value.split("")).forEach((data) ->{
+        Arrays.stream(value.split("")).forEach((data) -> {
             box.put(data, box.getOrDefault(data, 0) + 1);
         });
 
@@ -36,35 +35,21 @@ public class Solve {
     }
 
     private static void palindrome(Map<String, Integer> box) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         String leftValue = "";
         for (String s : box.keySet()) {
+            int loopCount = box.get(s) / 2;
+
+            for (int i = 0; i < loopCount; i++) {
+                result.append(s);
+            }
             if (box.get(s) % 2 != 0) {
-                int loopCount = box.get(s) / 2;
-
-                for (int i = 0; i < loopCount; i++) {
-                    result += s;
-                }
                 leftValue = s;
-
-            } else {
-                int loopCount = box.get(s) / 2;
-
-                for (int i = 0; i < loopCount; i++) {
-                    result += s;
-                }
             }
-
-
         }
-            String[] split = result.split("");
-            result += leftValue;
+        System.out.println(result.toString() + leftValue + result.reverse().toString());
 
-            for(int i = split.length-1; i >= 0; i--) {
-                result += split[i];
-            }
-            System.out.println(result);
 
     }
 }
