1c82d35e0eb5085d037c76ec02cbe54b70bea2d5
chhong <81909140+pnci1029@users.noreply.github.com>
2025-02-24 14:55:58 +0900

    Update 250224_11.java

--- src/main/java/leetcode/two_ponters/250224_11.java
+++ src/main/java/leetcode/two_ponters/250224_11.java
@@ -1,11 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
-        
-        // int first = height[0];
-        // int last = height[height.length -1];
 
-        // System.out.println(first);
-        // System.out.println(last);
         int first = 0;
         int last = height.length -1;
         int result = 0;
@@ -14,7 +9,6 @@ class Solution {
 
             int width = last - first;
             int heights = (Math.min(height[last], height[first]));
-            System.out.println("height : " +  heights);
             result = Math.max(result, width * heights);
 
             if(height[first] > height[last]){
@@ -22,8 +16,6 @@ class Solution {
             }else{
                 first++;
             }
-            System.out.println(result);
-            
         }
 
         return result;
