364a41f7f937e521018370f631c824e60f295de4
chhong <81909140+pnci1029@users.noreply.github.com>
2025-02-28 14:03:25 +0900

    Update 250228_1456.java

--- src/main/java/leetcode/sliding_window/250228_1456.java
+++ src/main/java/leetcode/sliding_window/250228_1456.java
@@ -1,27 +1,33 @@
-v1
 class Solution {
     public int maxVowels(String s, int k) {
         
         String word = "";
-        int vowelCount = 0;
+        int count = 0;
         String[] splits = s.split("");
-        String [] vowels = ["a", "e", "i", "o", "u"];
-        List<String> box = new ArrayList<>();
+        List<String> vowels = List.of("a", "e", "i", "o", "u");
+        List<Integer> vowelCounts = new ArrayList<>();
         for(int i =0; i<k; i++){
             word += splits[i];
-        
+            if(vowels.contains(splits[i])){
+                count++;
+            }
         }
-        box.add(word);
+        vowelCounts.add(count);
 
-        for(int i =1; i<s.length()-k; i++){
+        for(int i =1; i<s.length()-k +1; i++){
+            if(vowels.contains(word.substring(0,1))){
+                count--;
+            }
             word = word.substring(1)+splits[i + k -1];
-            box.add(word);
+            
+            if(vowels.contains(splits[i + k -1])){
+                count++;
+            }
+            vowelCounts.add(count);
         }
+        Collections.sort(vowelCounts);
+        Collections.reverse(vowelCounts);
 
-        System.out.println(Arrays.toString(splits));
-        System.out.println("box : " + box);
-
-
-        return 0;
+        return vowelCounts.get(0);
     }
 }
