5fff43bcb9c4f361bcb258964acaed9077397c65
chhong <81909140+pnci1029@users.noreply.github.com>
2025-03-12 17:04:32 +0900

    Update 250312_1657.java

--- src/main/java/leetcode/hashMap&Set/250312_1657.java
+++ src/main/java/leetcode/hashMap&Set/250312_1657.java
@@ -30,3 +30,43 @@ class Solution {
         return resultA == resultB && setA.equals(setB);
     }
 }
+
+
+v2
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        // 길이가 다르면 바로 false 반환
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        
+        // 각 문자의 출현 빈도수 계산
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        // 문자 존재 여부 확인용 비트마스크
+        int mask1 = 0;
+        int mask2 = 0;
+        
+        for (int i = 0; i < word1.length(); i++) {
+            freq1[word1.charAt(i) - 'a']++;
+            freq2[word2.charAt(i) - 'a']++;
+            
+            // 비트마스크로 각 문자의 존재 여부 표시
+            mask1 |= (1 << (word1.charAt(i) - 'a'));
+            mask2 |= (1 << (word2.charAt(i) - 'a'));
+        }
+        
+        // 두 문자열에 동일한 종류의 문자가 있는지 확인
+        if (mask1 != mask2) {
+            return false;
+        }
+        
+        // 빈도수 정렬 및 비교
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        
+        // 빈도수 배열 비교
+        return Arrays.equals(freq1, freq2);
+    }
+}
