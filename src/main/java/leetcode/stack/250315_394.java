42fa8ff4b95ad5aa909d87cb9d640a3a09e58c0d
chhong <81909140+pnci1029@users.noreply.github.com>
2025-03-17 11:46:08 +0900

    Update 250315_394.java

--- src/main/java/leetcode/stack/250315_394.java
+++ src/main/java/leetcode/stack/250315_394.java
@@ -1,3 +1,43 @@
+v2
+class Solution {
+    public String decodeString(String s) {
+        Stack<StringBuilder> stringBox = new Stack<>();
+        Stack<Integer> numberBox = new Stack<>();
+        
+        int currentNumber = 0;
+        StringBuilder result = new StringBuilder();
+
+        for(char value: s.toCharArray()){
+            // System.out.println(stringBox);
+            if(Character.isDigit(value)){
+                currentNumber = currentNumber * 10 + (value - '0');
+            }else if(value =='['){
+                numberBox.push(currentNumber);
+                stringBox.push(result);
+                result = new StringBuilder();
+                currentNumber = 0;
+            }else if(value == ']'){
+                System.out.println("stringBox : "+stringBox);
+                int number = numberBox.pop();
+                StringBuilder text = stringBox.pop();
+                
+                System.out.println("number : "+number);
+                System.out.println("text : "+text);
+                for(int i =0; i<number; i++){
+                    text.append(result);
+                }
+                result = text;
+            }else {
+                System.out.println("value : "+value);
+                result.append(value);
+            }
+
+        }
+
+        return result.toString();
+    }
+}
+
 v1
 class Solution {
     public String decodeString(String s) {
