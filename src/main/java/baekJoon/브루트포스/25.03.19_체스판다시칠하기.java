d94f5a5c4406cbb9cd54eca93462e57a1b8ced18
chhong <81909140+pnci1029@users.noreply.github.com>
2025-03-19 18:14:36 +0900

    Update 25.03.19_체스판다시칠하기.java

--- src/main/java/baekJoon/브루트포스/25.03.19_체스판다시칠하기.java
+++ src/main/java/baekJoon/브루트포스/25.03.19_체스판다시칠하기.java
@@ -1,3 +1,111 @@
+v2
+package com.example.vargenerator.api.controller;
+
+import java.util.*;
+
+/**
+8 8
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBBBWBW
+WBWBWBWB
+BWBWBWBW
+WBWBWBWB
+BWBWBWBW
+<p>
+<p>
+10 13
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+BBBBBBBBWBWBW
+BBBBBBBBBWBWB
+WWWWWWWWWWBWB
+WWWWWWWWWWBWB
+<p>
+9 23
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBB
+BBBBBBBBBBBBBBBBBBBBBBW
+ */
+public class Solve {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int result = Integer.MAX_VALUE;
+
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
+
+        char[][] box = new char[row][column];
+
+
+        for (int i = 0; i < row; i++) {
+            box[i] = scanner.next().toCharArray();
+        }
+
+        for (int i = 0; i <= row - 8; i++) {
+            for (int j = 0; j <= column - 8; j++) {
+                int counts = countValues(box, i, j);
+                result = Math.min(result, counts);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    public static int countValues(char[][] board, int row, int column) {
+        int bCount = 0;
+        int wCount = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                char currentColor = board[row + i][column + j];
+
+                if ((i + j) % 2 == 0) { // 짝수
+                    if (currentColor == 'B') {
+                        bCount++;
+                    }
+                    if (currentColor == 'W') {
+                        wCount++;
+                    }
+                } else { // 홀수
+                    if (currentColor == 'W') {
+                        bCount++;
+                    }
+                    if (currentColor == 'B') {
+                        wCount++;
+                    }
+                }
+
+            }
+        }
+        return Math.min(bCount, wCount);
+    }
+
+
+    public static char changeValue(char value) {
+        if (value == 'B') {
+            return 'W';
+        } else {
+            return 'B';
+        }
+    }
+}
+
+
+
 v1 NOT SOLVED
 package com.example.vargenerator.api.controller;
 
